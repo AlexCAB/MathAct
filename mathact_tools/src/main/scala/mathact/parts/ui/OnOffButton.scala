c4b7ea91cbfe9126ee0753f12b16234e18236cd5
CAB <cabsoftbox@gmail.com>
2016-11-20 14:10:56 +0200

    0.2.38 ( work on AnalogGenerator tool)

--- mathact_tools/src/main/scala/mathact/parts/ui/OnOffButton.scala
+++ mathact_tools/src/main/scala/mathact/parts/ui/OnOffButton.scala
@@ -12,34 +12,38 @@
  * @                                                                             @ *
 \* *  http://github.com/alexcab  * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-package examples.tools.generators
+package mathact.parts.ui
 
-import mathact.core.bricks.linking.LinkIn
-import mathact.core.bricks.plumbing.wiring.fun.FunWiring
-import mathact.data.analog.Sample
-import mathact.tools.EmptyBlock
-import mathact.tools.generators.AnalogGenerator
-import mathact.tools.workbenches.SimpleWorkbench
+import scalafx.Includes._
+import scalafx.scene.control.Button
+import scalafx.scene.image.{ImageView, Image}
 
-
-/** Example of using AnalogGenerator tool
-  * Created by CAB on 19.11.2016.
+/** On and Off button
+  * Created by CAB on 20.11.2016.
   */
 
-class AnalogGeneratorExample extends SimpleWorkbench {
-  //Sketch parameters
-  heading = " Analog generator example"
-  //Blocks
-  val generator = new AnalogGenerator{
-    //Params
-    name = "Analog generator of sin(x)"
-    sampleRate = 100 //Hertz
-    period = 500     //Milliseconds
-    //Gen function
-    f = (t) ⇒ math.sin(t * math.Pi)}
-  val logger =  new EmptyBlock with FunWiring with LinkIn[Sample]{
-    name = "Logger"
-    val in = In[Sample]
-    in.foreach(v ⇒ logger.info("Logger received: " + v))}
-  //Connecting
-  generator ~> logger}
+class OnOffButton(onImg: Image, offImg: Image, disabledImg: Image)(doOn: ⇒Unit, doOff: ⇒Unit) extends Button {
+  //Images
+  private val onView = new ImageView{ image = onImg }
+  private val offView = new ImageView{ image = offImg }
+  private val dView = new ImageView{ image = disabledImg }
+  //Variables
+  private var isOn = false
+  //Config
+  graphic = dView
+  disable = true
+  onAction = handle{
+    passive()
+    if (isOn) doOn else doOff}
+  //Methods
+  def on(): Unit = {
+    graphic = onView
+    disable = false
+    isOn = true}
+  def off(): Unit = {
+    graphic = offView
+    disable = false
+    isOn = false}
+  def passive(): Unit = {
+    graphic = dView
+    disable = true}}
--- mathact_tools/src/main/scala/mathact/tools/generators/AnalogGenerator.scala
+++ mathact_tools/src/main/scala/mathact/tools/generators/AnalogGenerator.scala
@@ -21,11 +21,14 @@ import mathact.core.bricks.ui.BlockUI
 import mathact.core.bricks.ui.interaction.UIEvent
 import mathact.data.analog.Sample
 import mathact.data.ui.C
+import mathact.parts.ui.OnOffButton
 import mathact.tools.Tool
 
+import scala.concurrent.Future
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
 import scalafx.scene.control.{Label, SpinnerValueFactory, Spinner}
+import scalafx.scene.image.Image
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color
 
@@ -38,26 +41,60 @@ abstract class AnalogGenerator(implicit blockContext: BlockContext)
 extends Tool(blockContext, "AG", "mathact/tools/generators/analog_generator.png")
 with ObjWiring with ObjOnStart with ObjOnStop with BlockUI with LinkOut[Sample]{
   //Parameters
-  val defaultSampleRate: Long = 10  //In hertz
-  val minSampleRate: Long = 1
-  val maxSampleRate: Long = 1000
-  val defaultPeriod: Long = 1000  //In milliseconds
-  val minPeriod: Long = 1
-  val maxPeriod: Long = 1000000
+  val defaultSampleRate: Int = 10  //In hertz
+  val minSampleRate: Int = 1
+  val maxSampleRate: Int = 1000
+  val defaultPeriod: Int = 1000  //In milliseconds
+  val minPeriod: Int = 1
+  val maxPeriod: Int = 1000000
+  val defaultInitOn: Boolean = false
   val defaultF: Double⇒Double = _ ⇒ 0.0
   val sampleRateStep: Int = 10
   val periodStep: Int = 10
   val uiElemsHeight: Int = 25
+  val uiBtnSize: Int = 25
   val uiSpinnerWidth: Int = 100
   val uiLabelStyle: String =  "-fx-font-weight: bold; -fx-font-size: 11pt;"
+  //Resources
+  private val onBtnImg = new Image("mathact/tools/generators/on_off_button_on.png", uiBtnSize, uiBtnSize, true, true)
+  private val offBtnImg = new Image("mathact/tools/generators/on_off_button_off.png", uiBtnSize, uiBtnSize, true, true)
+  private val disableBtnImg = new Image("mathact/tools/generators/on_off_button_d.png", uiBtnSize, uiBtnSize, true, true)
   //Properties
   @volatile private var _sampleRate = defaultSampleRate
   @volatile private var _period = defaultPeriod
+  @volatile private var _initOn = defaultInitOn
   @volatile private var _f = defaultF
   //Definitions
-  private case class SampleRateChanged(sampleRate: Long) extends UIEvent
-  private case class PeriodChanged(period: Long) extends UIEvent
+  private case class SetGen(sampleRate: Int, period: Int) extends UIEvent
+  private case object StopGen extends UIEvent
+  private class Gen(sampleRate: Int, period: Int, function: Double⇒Double, outflow: {def riseEvent(s: Sample): Unit}){
+    //Variables
+    @volatile private var work = true
+    //Methods
+    def stop(): Unit = { work = false }
+    //Worker
+    Future{
+
+
+      ???
+
+
+
+
+
+    }
+
+
+
+
+  }
   //Variables
+  @volatile private var currentGen: Option[Gen] = None
+
+
+
+
+
 
 
 
@@ -65,7 +102,7 @@ with ObjWiring with ObjOnStart with ObjOnStop with BlockUI with LinkOut[Sample]{
   //UI definition
   UI{ new SfxFrame{
     //Params
-    title = "Analog generator" + (name match{case Some(n) ⇒ " - " + n case _ ⇒ ""})
+    val initTitle = "Analog generator" + (name match{case Some(n) ⇒ " - " + n case _ ⇒ ""})
     showOnStart = true
     resizable = false
     //Bounds
@@ -74,31 +111,39 @@ with ObjWiring with ObjOnStart with ObjOnStop with BlockUI with LinkOut[Sample]{
     val period =
       if(_period < minPeriod) minPeriod else if(_period > maxPeriod ) maxPeriod else _period
     //Components
-    val spinnerSampleRate = new Spinner[Long]{
+    val spinnerSampleRate = new Spinner[Int]{
       prefHeight = uiElemsHeight
       prefWidth = uiSpinnerWidth
       style = "-fx-font-size: 11pt;"
       disable = true
       editable = true
-      value.onChange{sendEvent(SampleRateChanged(this.value.value))}
+      value.onChange{ if(! disable.value) setGen() }
       valueFactory = new SpinnerValueFactory
-        .DoubleSpinnerValueFactory(minSampleRate, maxSampleRate, sampleRate, sampleRateStep)
-        .asInstanceOf[SpinnerValueFactory[Long]]}
-    val spinnerPeriod = new Spinner[Long]{
+        .IntegerSpinnerValueFactory(minSampleRate, maxSampleRate, sampleRate, sampleRateStep)
+        .asInstanceOf[SpinnerValueFactory[Int]]}
+    val spinnerPeriod = new Spinner[Int]{
       prefHeight = uiElemsHeight
       prefWidth = uiSpinnerWidth
       style = "-fx-font-size: 11pt;"
       disable = true
       editable = true
-      value.onChange{sendEvent(PeriodChanged(this.value.value))}
+      value.onChange{ if(! disable.value) setGen() }
       valueFactory = new SpinnerValueFactory
-        .DoubleSpinnerValueFactory(minPeriod, maxPeriod, period, periodStep)
-        .asInstanceOf[SpinnerValueFactory[Long]]}
-
-
-
-
-
+        .IntegerSpinnerValueFactory(minPeriod, maxPeriod, period, periodStep)
+        .asInstanceOf[SpinnerValueFactory[Int]]}
+    val onOffBtn: OnOffButton = new OnOffButton(onBtnImg, offBtnImg, disableBtnImg)(
+      doOn = {
+        onOffBtn.off()
+        setGen()},
+      doOff = {
+        onOffBtn.on()
+        setTitle(isOn = false)
+        sendEvent(StopGen)})
+    //Functions
+    def setTitle(isOn: Boolean): Unit = { title = initTitle + " | " + (if (isOn) "ON" else "OFF") }
+    def setGen(): Unit = {
+      setTitle(isOn = true)
+      sendEvent( SetGen(spinnerSampleRate.value.value, spinnerPeriod.value.value))}
     //Scene
     scene = new Scene{
       fill = Color.White
@@ -110,50 +155,45 @@ with ObjWiring with ObjOnStart with ObjOnStop with BlockUI with LinkOut[Sample]{
             text = "Sample rate (Hz) = "
             style = uiLabelStyle},
           spinnerSampleRate,
-          new Label{9
+          new Label{
             text = ", Period (ms) = "
             style = uiLabelStyle},
-          spinnerPeriod)}}
+          spinnerPeriod,
+          onOffBtn)}
+      setTitle(_initOn)}
     //Commands reactions
     def onCommand = {
       case C.Start ⇒
-//        sendEvent(E.LongValueChanged((1000 * precision) / initFreq)) //Sends in milli seconds
-//        startBtn.active()
-//        stepBtn.active()
-//        speedSlider.disable = false
+        spinnerSampleRate.disable = false
+        spinnerPeriod.disable = false
+        if (_initOn) onOffBtn.off() else onOffBtn.on()
+        if (_initOn) sendEvent( SetGen(spinnerSampleRate.value.value, spinnerPeriod.value.value))
       case C.Stop ⇒
-//        sendEvent(E.Stop)
-//        startBtn.passive()
-//        stopBtn.passive()
-//        stepBtn.passive()
-//        speedSlider.disable = true
-    }}}
-
-
-
-
-
+        spinnerSampleRate.disable = true
+        spinnerPeriod.disable = true
+        onOffBtn.passive()
+        sendEvent(StopGen)}}}
   //Outflow
-  private val outflow = new Outflow[Sample]{
-    def riseEvent(): Unit = pour(???)}
-
-
+  private val outflow = new Outflow[Sample]{ def riseEvent(s: Sample): Unit = pour(s) }
   //On start and on stop
   protected def onStart(): Unit = { UI.sendCommand(C.Start) }
   protected def onStop(): Unit = { UI.sendCommand(C.Stop) }
-
   //UI handling
-  UI.onEvent{ case e ⇒ ??? }
-
-
-
-
+  UI.onEvent{
+    case SetGen(sampleRate, period) ⇒
+      currentGen.foreach(_.stop())
+      currentGen = Some(new Gen(sampleRate, period, _f, outflow))
+    case StopGen ⇒
+      currentGen.foreach(_.stop())
+      currentGen = None}
   //DSL
-  def sampleRate: Long = _sampleRate
-  def sampleRate_=(v: Long){ _sampleRate = v }
-  def period: Long = _period
-  def period_=(v: Long){ _period = v}
+  def sampleRate: Int = _sampleRate
+  def sampleRate_=(v: Int){ _sampleRate = v }
+  def period: Int = _period
+  def period_=(v: Int){ _period = v }
+  def initOn: Boolean = _initOn
+  def initOn_=(v: Boolean){ _initOn = v }
   def f: Double⇒Double = _f
-  def f_=(v: Double⇒Double){ _f = v}
+  def f_=(v: Double⇒Double){ _f = v }
   //Output
   val out = Outlet[Sample](outflow)}
